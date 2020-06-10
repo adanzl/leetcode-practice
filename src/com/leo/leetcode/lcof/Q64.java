3bc239834a79e06674be6e19431c321e43d3399e
zhaolin09 <zhaolin09@58.com>
2020-06-10 11:48:04 +0800

    lcof-Q64

---
+++ src/com/leo/leetcode/lcof/Q64.java
@@ -0,0 +1,14 @@
+package com.leo.leetcode.lcof;
+
+public class Q64 {
+    public static void main(String[] args) {
+        System.out.println(new Q64().sumNums(3)); // 6
+        System.out.println(new Q64().sumNums(9)); // 45
+    }
+
+    int sumNums(int n) {
+        int ans = n;
+        boolean b = (ans > 0 && (ans += sumNums(n - 1)) > 0);
+        return ans;    // 短路特性，&&前面为假，后面不计算
+    }
+}
