17d1dae884d91c2bff7ba8438b42cbdccbeca4fc
zhaolin <zhaolin@pythonera.com>
2020-06-21 00:21:17 +0800

    加入注释

--- src/com/leo/leetcode/algorithm/Q124.java
+++ src/com/leo/leetcode/algorithm/Q124.java
@@ -3,6 +3,11 @@ package com.leo.leetcode.algorithm;
 import com.leo.utils.LCUtil;
 import com.leo.utils.TreeNode;
 
+/**
+ * 给定一个非空二叉树，返回其最大路径和。
+ * 本题中，路径被定义为一条从树中任意节点出发，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
+ * 链接：https://leetcode-cn.com/problems/binary-tree-maximum-path-sum
+ */
 public class Q124 {
 
     public static void main(String[] args) {
--- src/com/leo/leetcode/algorithm/Q24.java
+++ src/com/leo/leetcode/algorithm/Q24.java
@@ -1,8 +1,19 @@
 package com.leo.leetcode.algorithm;
 
+import com.leo.utils.LCUtil;
 import com.leo.utils.ListNode;
 
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+ */
+
 public class Q24 {
+    public static void main(String[] args) {
+        System.out.println(new Q24().swapPairs(LCUtil.stringToListNode("[1,2,3,4]"))); // [2,1,4,3]
+    }
+
     public ListNode swapPairs(ListNode head) {
         ListNode h = new ListNode(0);
         h.next = head;
