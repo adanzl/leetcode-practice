0b4291b141d358cdbe9d49ecce3e172e12ccc25c
zhaolin <zhaolin@pythonera.com>
2020-06-23 00:32:59 +0800

    add main

--- src/com/leo/leetcode/algorithm/Q64.java
+++ src/com/leo/leetcode/algorithm/Q64.java
@@ -1,6 +1,15 @@
 package com.leo.leetcode.algorithm;
 
+/**
+ * 给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ * 说明：每次只能向下或者向右移动一步。
+ * 链接：https://leetcode-cn.com/problems/minimum-path-sum/
+ */
 public class Q64 {
+    public static void main(String[] args) {
+        new Q64().TestOJ();
+    }
+
     public void TestOJ() {
         System.out.println(minPathSum(new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}})); // 7
     }
