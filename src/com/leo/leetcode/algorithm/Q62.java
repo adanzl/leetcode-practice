1f93e83828e7e5b36ff34a83f3bdd1b2960a0a65
zhaolin <zhaolin@pythonera.com>
2020-06-23 00:33:52 +0800

    add main

--- src/com/leo/leetcode/algorithm/Q62.java
+++ src/com/leo/leetcode/algorithm/Q62.java
@@ -1,6 +1,16 @@
 package com.leo.leetcode.algorithm;
 
+/**
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * 问总共有多少条不同的路径？
+ * 链接：https://leetcode-cn.com/problems/unique-paths
+ */
 public class Q62 {
+    public static void main(String[] args) {
+        new Q62().TestOJ();
+    }
+
     public void TestOJ() {
         System.out.println(uniquePaths(3, 2)); // 3
         System.out.println(uniquePaths(7, 3)); // 28
