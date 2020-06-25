9841dc4e7cd92c5d4c2ec2b9aaa018b00c4c1e53
zhaolin <zhaolin@pythonera.com>
2020-06-26 00:49:21 +0800

    add main

--- src/com/leo/leetcode/lcci/Q0101.java
+++ src/com/leo/leetcode/lcci/Q0101.java
@@ -1,5 +1,9 @@
 package com.leo.leetcode.lcci;
 
+/**
+ * 实现一个算法，确定一个字符串 s 的所有字符是否全都不同。
+ * 链接：https://leetcode-cn.com/problems/is-unique-lcci/
+ */
 public class Q0101 {
     public static void main(String[] args) {
         System.out.println(new Q0101().isUnique("leetcode")); // false
