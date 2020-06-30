eb82240e6217fe31c0c0e3b9bcc36ba4b8d78149
zhaolin <zhaolin@pythonera.com>
2020-07-01 00:57:07 +0800

    add comment

--- src/com/leo/leetcode/algorithm/Q17.java
+++ src/com/leo/leetcode/algorithm/Q17.java
@@ -4,7 +4,18 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ * 说明:
+ * 尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+ * 链接：https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number
+ */
 public class Q17 {
+    public static void main(String[] args) {
+        System.out.println(new Q17().letterCombinations("23")); // ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+    }
+
     public List<String> letterCombinations(String digits) {
 
         HashMap<Character, String[]> charMap = new HashMap<>();
