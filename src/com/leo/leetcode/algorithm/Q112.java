3f652d75951243d14836114548a41627728de669
zhaolin <zhaolin@pythonera.com>
2020-07-10 01:41:03 +0800

    add comment

--- src/com/leo/leetcode/algorithm/Q112.java
+++ src/com/leo/leetcode/algorithm/Q112.java
@@ -3,6 +3,11 @@ package com.leo.leetcode.algorithm;
 import com.leo.utils.LCUtil;
 import com.leo.utils.TreeNode;
 
+/**
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 链接：https://leetcode-cn.com/problems/path-sum/
+ */
 public class Q112 {
     public static void main(String[] args) {
         new Q112().TestOJ();
