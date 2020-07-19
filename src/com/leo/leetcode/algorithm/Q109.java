0f4740968f129d3930e4f0f900d81b333d0b0ff3
zhaolin <zhaolin@pythonera.com>
2020-07-20 00:12:56 +0800

    Q167

--- src/com/leo/leetcode/algorithm/Q109.java
+++ src/com/leo/leetcode/algorithm/Q109.java
@@ -4,9 +4,6 @@ import com.leo.utils.LCUtil;
 import com.leo.utils.ListNode;
 import com.leo.utils.TreeNode;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 /**
  * 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
  * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
@@ -16,13 +13,15 @@ import java.util.Queue;
 public class Q109 {
     public static void main(String[] args) {
         // [0,-3,9,-10,null,5]
-        System.out.println(LCUtil.treeNodeToString(new Q109().sortedListToBST(LCUtil.stringToListNode("[-10,-3,0,5,9]"))));
+        System.out.println(
+                LCUtil.treeNodeToString(new Q109().sortedListToBST(LCUtil.stringToListNode("[-10,-3,0,5,9]"))));
     }
 
     ListNode pNode;
 
     public TreeNode sortedListToBST(ListNode head) {
-        if (head == null) return null;
+        if (head == null)
+            return null;
         ListNode p = head;
         int count = 0;
         while (p != null) {
@@ -35,7 +34,8 @@ public class Q109 {
     }
 
     TreeNode buildTree(int l, int r) {
-        if (l == r) return null;
+        if (l == r)
+            return null;
         int mid = (l + r) >> 1;
         TreeNode root = new TreeNode(0);
         root.left = buildTree(l, mid);
---
+++ src/com/leo/leetcode/algorithm/Q167.java
@@ -0,0 +1,28 @@
+package com.leo.leetcode.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+ * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+ * 说明:
+ * 返回的下标值（index1 和 index2）不是从零开始的。
+ * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+ * 链接：https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted
+ */
+public class Q167 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Q167().twoSum(new int[]{2, 7, 11, 15}, 9))); // [1,2]
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.length;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r - 1];
+            if (sum == target) break;
+            else if (sum < target) l++;
+            else r--;
+        }
+        return new int[]{l + 1, r};
+    }
+}
