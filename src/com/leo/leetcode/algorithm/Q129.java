953d3ec73c915adca9b2f7d05fc14167c104db51
zhaolin09 <zhaolin09@58.com>
2020-07-23 19:35:40 +0800

    Q130

--- src/com/leo/leetcode/algorithm/Q129.java
+++ src/com/leo/leetcode/algorithm/Q129.java
@@ -3,9 +3,6 @@ package com.leo.leetcode.algorithm;
 import com.leo.utils.LCUtil;
 import com.leo.utils.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
  * 例如，从根到叶子节点路径 1->2->3 代表数字 123。
