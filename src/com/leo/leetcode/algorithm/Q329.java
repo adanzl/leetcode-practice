2f8a7217b4c7a753b78390a2a68ac16082461256
zhaolin09 <zhaolin09@58.com>
2020-07-27 13:58:37 +0800

    Q329

--- src/com/leo/leetcode/algorithm/Q329.java
+++ src/com/leo/leetcode/algorithm/Q329.java
@@ -1,5 +1,7 @@
 package com.leo.leetcode.algorithm;
 
+import java.util.Arrays;
+
 import com.leo.utils.LCUtil;
 
 /**
@@ -7,15 +9,39 @@ import com.leo.utils.LCUtil;
  * 你不能在对角线方向上移动或移动到边界外（即不允许环绕）。
  * 链接：https://leetcode-cn.com/problems/longest-increasing-path-in-a-matrix/
  */
-// TODO
 class Q329 {
 
     public static void main(String[] args) {
+        System.out.println(new Q329().longestIncreasingPath(LCUtil.stringToInt2dArray("[[2147483647,1]]"))); // 2
         System.out.println(new Q329().longestIncreasingPath(LCUtil.stringToInt2dArray("[[9,9,4],[6,6,8],[2,1,1]]"))); // 4
         System.out.println(new Q329().longestIncreasingPath(LCUtil.stringToInt2dArray("[[3,4,5],[3,2,6],[2,2,1]]"))); // 4
     }
 
     public int longestIncreasingPath(int[][] matrix) {
-        return 0;
+        if (matrix.length == 0) return 0;
+        int[][] marks = new int[matrix.length][matrix[0].length];
+        for (int[] l : marks) {
+            Arrays.fill(l, -1);
+        }
+        int out = 0;
+        for (int i = 0; i < marks.length; i++) {
+            for (int j = 0; j < marks[i].length; j++) {
+                out = Math.max(out, process(matrix, marks, i, j, Integer.MAX_VALUE + 1L));
+            }
+        }
+        return out;
+    }
+
+    int process(int[][] matrix, int[][] marks, int x, int y, long pre_v) {
+        if (x < 0 || x >= marks.length || y < 0 || y >= marks[0].length || matrix[x][y] >= pre_v) return 0;
+        if (marks[x][y] != -1) return marks[x][y];
+        int out = 0, v = matrix[x][y];
+        out = Math.max(out, process(matrix, marks, x - 1, y, v));
+        out = Math.max(out, process(matrix, marks, x + 1, y, v));
+        out = Math.max(out, process(matrix, marks, x, y + 1, v));
+        out = Math.max(out, process(matrix, marks, x, y - 1, v));
+        ++out;
+        marks[x][y] = out;
+        return out;
     }
 }
