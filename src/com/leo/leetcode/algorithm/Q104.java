d8701fa935ccdea239c2c6a40383da874eb5beb0
zhaolin <zhaolin@pythonera.com>
2020-07-28 00:02:27 +0800

    add comment

--- src/com/leo/leetcode/algorithm/Q104.java
+++ src/com/leo/leetcode/algorithm/Q104.java
@@ -3,6 +3,12 @@ package com.leo.leetcode.algorithm;
 import com.leo.utils.LCUtil;
 import com.leo.utils.TreeNode;
 
+/**
+ * 给定一个二叉树，找出其最大深度。
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ */
 public class Q104 {
 
     public static void main(String[] args) {
@@ -14,9 +20,7 @@ public class Q104 {
     }
 
     public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
+        if (root == null) return 0;
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
