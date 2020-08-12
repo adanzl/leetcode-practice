5a9c734bbdf096b984e0b806ecdc9ae9cf44b8d5
zhaolin <zhaolin@pythonera.com>
2020-08-13 00:33:00 +0800

    add comment

--- src/com/leo/leetcode/algorithm/Q43.java
+++ src/com/leo/leetcode/algorithm/Q43.java
@@ -1,6 +1,16 @@
 package com.leo.leetcode.algorithm;
 
+/**
+ * 给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+ * 说明：
+ *  num1 和 num2 的长度小于110。
+ *  num1 和 num2 只包含数字 0-9。
+ *  num1 和 num2 均不以零开头，除非是数字 0 本身。
+ *  不能使用任何标准库的大数类型（比如 BigInteger）或直接将输入转换为整数来处理。
+ * 链接：https://leetcode-cn.com/problems/multiply-strings
+ */
 public class Q43 {
+
     public void TestOJ() {
         System.out.println(multiply("9", "9")); // "81"
         System.out.println(multiply("123", "456")); // "56088"
