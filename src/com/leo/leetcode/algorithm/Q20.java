35c743be4a9c741aac54210f90257abc38b94374
zhaolin <zhaolin@pythonera.com>
2020-08-14 02:04:13 +0800

    Q20

--- src/com/leo/leetcode/algorithm/Q20.java
+++ src/com/leo/leetcode/algorithm/Q20.java
@@ -1,30 +1,41 @@
 package com.leo.leetcode.algorithm;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
-
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 有效字符串需满足：
+ *   左括号必须用相同类型的右括号闭合。
+ *   左括号必须以正确的顺序闭合。
+ *   注意空字符串可被认为是有效字符串。
+ * 链接：https://leetcode-cn.com/problems/valid-parentheses
+ */
 public class Q20 {
+
+    public static void main(String[] args) {
+        System.out.println(new Q20().isValid("()")); // true
+        System.out.println(new Q20().isValid("()[]{}")); // true
+        System.out.println(new Q20().isValid("(]")); // false
+        System.out.println(new Q20().isValid("([)]")); // false
+        System.out.println(new Q20().isValid("{[]}")); // true
+    }
+
     public boolean isValid(String s) {
-        Map<Character, Character> charMap = new HashMap<>();
-        charMap.put('[', ']');
-        charMap.put('{', '}');
-        charMap.put('(', ')');
-        Stack<Character> stack = new Stack<>();
+        char[] stack = new char[s.length()];
+        int index = 0;
         for (int i = 0; i < s.length(); i++) {
             char v = s.charAt(i);
             if (' ' == v) continue;
-            if (charMap.containsKey(v)) {
-                stack.push(v);
+            if (v == '[' || v == '(' || v == '{') {
+                stack[index++] = v;
             } else {
-                if (stack.empty()) return false;
-                char c = stack.pop();
-                if (charMap.get(c) != v) {
-                    return false;
-                }
+                if (index == 0) return false;
+                char c = stack[--index];
+                char r = ' ';
+                if (c == '[') r = ']';
+                else if (c == '{') r = '}';
+                else if (c == '(') r = ')';
+                if (r != v) return false;
             }
         }
-
-        return stack.empty();
+        return index == 0;
     }
 }
