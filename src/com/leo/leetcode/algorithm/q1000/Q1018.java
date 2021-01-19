53f205b96913d5277ceb2849ca644ad21f077bce
zhaolin09 <zhaolin09@58.com>
2021-01-19 13:45:12 +0800

    补全描述

--- src/com/leo/leetcode/algorithm/q1000/Q1018.java
+++ src/com/leo/leetcode/algorithm/q1000/Q1018.java
@@ -5,6 +5,12 @@ import static com.leo.utils.LCUtil.stringToIntegerArray;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 给定由若干 0 和 1 组成的数组 A。我们定义 N_i：从 A[0] 到 A[i] 的第 i 个子数组被解释为一个二进制数（从最高有效位到最低有效位）。
+ * 返回布尔值列表 answer，只有当 N_i 可以被 5 整除时，答案 answer[i] 为 true，否则为 false。
+ *
+ * 链接：https://leetcode-cn.com/problems/binary-prefix-divisible-by-5
+ */
 public class Q1018 {
     public static void main(String[] args) {
         // [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,true,true,true,true,false]
