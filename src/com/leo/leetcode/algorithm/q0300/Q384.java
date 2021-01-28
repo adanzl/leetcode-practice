9ef1be4d25e3ddaaac77b803ef3ed1519a43e7a5
zhaolin <zhaolin@pythonera.com>
2021-01-28 23:52:27 +0800

    补充注释

--- src/com/leo/leetcode/algorithm/q0300/Q384.java
+++ src/com/leo/leetcode/algorithm/q0300/Q384.java
@@ -3,6 +3,21 @@ package com.leo.leetcode.algorithm.q0300;
 import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * 给你一个整数数组 nums ，设计算法来打乱一个没有重复元素的数组。
+ * 实现 Solution class:
+ * Solution(int[] nums) 使用整数数组 nums 初始化对象
+ * int[] reset() 重设数组到它的初始状态并返回
+ * int[] shuffle() 返回数组随机打乱后的结果
+ * <p>
+ * 提示：
+ * 1、1 <= nums.length <= 200
+ * 2、-10^6 <= nums[i] <= 10^6
+ * 3、nums 中的所有元素都是 唯一的
+ * 4、最多可以调用 5 * 104 次 reset 和 shuffle
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/shuffle-an-array
+ */
 public class Q384 {
 
     public static void main(String[] args) {
