2cbf60381fb281d5959097fc39a705d24efa0e88
zhaolin <zhaolin@pythonera.com>
2021-03-12 00:37:12 +0800

    opt-comment

--- src/com/leo/leetcode/algorithm/q0300/Q300.java
+++ src/com/leo/leetcode/algorithm/q0300/Q300.java
@@ -1,14 +1,31 @@
 package com.leo.leetcode.algorithm.q0300;
 
-import com.leo.utils.LCUtil;
+import static com.leo.utils.LCUtil.stringToIntegerArray;
 
 import java.util.Arrays;
 
+/**
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ * <p>
+ * 提示：
+ * 1、1 <= nums.length <= 2500
+ * 2、-10^4 <= nums[i] <= 10^4
+ * <p>
+ * 进阶：
+ * 1、你可以设计时间复杂度为 O(n2) 的解决方案吗？
+ * 2、你能将算法的时间复杂度降低到 O(n log(n)) 吗?
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/longest-increasing-subsequence
+ */
 public class Q300 {
-    public void TestOJ() {
-        System.out.println(lengthOfLIS(LCUtil.stringToIntegerArray("[10,9,2,5,3,4]"))); // 3
-        System.out.println(lengthOfLIS(LCUtil.stringToIntegerArray("[10,9,2,5,3,7,101,18]"))); // 4
-        System.out.println(lengthOfLIS(LCUtil.stringToIntegerArray("[0]"))); // 4
+    public static void main(String[] args) {
+        // 3
+        System.out.println(new Q300().lengthOfLIS(stringToIntegerArray("[10,9,2,5,3,4]")));
+        // 4
+        System.out.println(new Q300().lengthOfLIS(stringToIntegerArray("[10,9,2,5,3,7,101,18]")));
+        // 4
+        System.out.println(new Q300().lengthOfLIS(stringToIntegerArray("[0]")));
     }
 
     private int lengthOfLIS(int[] nums) {
--- src/com/leo/leetcode/algorithm/q0300/Q399.java
+++ src/com/leo/leetcode/algorithm/q0300/Q399.java
@@ -1,15 +1,38 @@
 package com.leo.leetcode.algorithm.q0300;
 
-import com.leo.utils.LCUtil;
-
 import java.util.*;
 
+import static com.leo.utils.LCUtil.stringToDoubleArray;
+import static com.leo.utils.LCUtil.stringToListListString;
+
+/**
+ * 给你一个变量对数组 equations 和一个实数值数组 values 作为已知条件，其中 equations[i] = [Ai, Bi] 和 values[i] 共同表示等式 Ai / Bi = values[i] 。
+ * 每个 Ai 或 Bi 是一个表示单个变量的字符串。
+ * 另有一些以数组 queries 表示的问题，其中 queries[j] = [Cj, Dj] 表示第 j 个问题，请你根据已知条件找出 Cj / Dj = ? 的结果作为答案。
+ * 返回 所有问题的答案 。如果存在某个无法确定的答案，则用 -1.0 替代这个答案。如果问题中出现了给定的已知条件中没有出现的字符串，也需要用 -1.0 替代这个答案。
+ * 注意：输入总是有效的。你可以假设除法运算中不会出现除数为 0 的情况，且不存在任何矛盾的结果。
+ * <p>
+ * 提示：
+ * 1、1 <= equations.length <= 20
+ * 2、equations[i].length == 2
+ * 3、1 <= Ai.length, Bi.length <= 5
+ * 4、values.length == equations.length
+ * 5、0.0 < values[i] <= 20.0
+ * 6、1 <= queries.length <= 20
+ * 7、queries[i].length == 2
+ * 8、1 <= Cj.length, Dj.length <= 5
+ * 9、Ai, Bi, Cj, Dj 由小写英文字母与数字组成
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/evaluate-division
+ */
 public class Q399 {
-    public void TestOJ() {
-        System.out.println(Arrays.toString(calcEquation(
-                LCUtil.stringToListListString("[[\"a\",\"b\"],[\"b\",\"c\"]]"),
-                LCUtil.stringToDoubleArray("[2.0,3.0]"),
-                LCUtil.stringToListListString("[[\"a\",\"c\"],[\"b\",\"a\"],[\"a\",\"e\"],[\"a\",\"a\"],[\"x\",\"x\"]]") // [6.0, 0.5, -1.0, 1.0, -1.0 ]
+
+    public static void main(String[] args) {
+        // [6.0, 0.5, -1.0, 1.0, -1.0 ]
+        System.out.println(Arrays.toString(new Q399().calcEquation(
+                stringToListListString("[[\"a\",\"b\"],[\"b\",\"c\"]]"),
+                stringToDoubleArray("[2.0,3.0]"),
+                stringToListListString("[[\"a\",\"c\"],[\"b\",\"a\"],[\"a\",\"e\"],[\"a\",\"a\"],[\"x\",\"x\"]]")
         )));
     }
 
