8b3550186482429cda938587af59c65981ff2498
zhaolin09 <zhaolin09@58.com>
2022-01-13 14:01:01 +0800

    FIX：fix log

--- src/com/leo/leetcode/algorithm/q0000/Q89.java
+++ src/com/leo/leetcode/algorithm/q0000/Q89.java
@@ -7,6 +7,9 @@ import java.util.List;
  * 格雷编码是一个二进制数字系统，在该系统中，两个连续的数值仅有一个位数的差异。
  * 给定一个代表编码总位数的非负整数 n，打印其格雷编码序列。即使有多个不同答案，你也只需要返回其中一种。
  * 格雷编码序列必须以 0 开头。
+ * 提示：
+ * 1 <= n <= 16
+ * <p>
  * 链接：https://leetcode-cn.com/problems/gray-code
  */
 public class Q89 {
@@ -17,6 +20,9 @@ public class Q89 {
         System.out.println(new Q89().grayCode(2)); // [0,1,3,2]
         System.out.println(new Q89().grayCode(3)); // [0,1,3,2,6,7,5,4]
         System.out.println(new Q89().grayCode(4)); // [0,1,3,2,6,7,5,4,12,13,15,14,10,11,9,8]
+        for (int i = 1; i <= 16; i++) {
+            System.out.println(new Q89().grayCode(i));
+        }
     }
 
     public List<Integer> grayCode(int n) {
