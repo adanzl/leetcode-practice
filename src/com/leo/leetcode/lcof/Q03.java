105ff1f543d3bfc00484dbe294e209dab4aacd79
zhaolin09 <zhaolin09@58.com>
2022-02-12 01:49:30 +0800

    lcof-Q03

--- src/com/leo/leetcode/lcof/Q03.java
+++ src/com/leo/leetcode/lcof/Q03.java
@@ -1,9 +1,19 @@
 package com.leo.leetcode.lcof;
 
+/**
+ * 找出数组中重复的数字。
+ * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+ * 限制：
+ * 1、2 <= n <= 100000
+ * 链接：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof
+ */
 public class Q03 {
 
     public static void main(String[] args) {
-        System.out.println(new Q03().findRepeatNumber(new int[]{2, 3, 1, 0, 2, 5, 3})); // 2 || 3
+        // 0
+        System.out.println(new Q03().findRepeatNumber(new int[]{3, 4, 2, 0, 0, 1}));
+        // 2 || 3
+        System.out.println(new Q03().findRepeatNumber(new int[]{2, 3, 1, 0, 2, 5, 3}));
     }
 
     public int findRepeatNumber(int[] nums) {
@@ -11,10 +21,12 @@ public class Q03 {
             int p = nums[i];
             while (p != i) {
                 int t = nums[p];
-                if (t == p) return p;
+                if (t == p)
+                    return p;
                 nums[p] = p;
                 p = t;
             }
+            nums[p] = i;
         }
         return -1;
     }
