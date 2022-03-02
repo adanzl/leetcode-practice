c9ce70c9e3dd0bc6514e88c85774a4639ba93aaa
zhaolin09 <zhaolin09@58.com>
2022-03-02 10:58:46 +0800

    Q673

--- src/com/leo/leetcode/algorithm/q0600/Q673.java
+++ src/com/leo/leetcode/algorithm/q0600/Q673.java
@@ -1,12 +1,12 @@
 package com.leo.leetcode.algorithm.q0600;
 
-import static com.leo.utils.LCUtil.stringToIntegerArray;
+import com.leo.utils.LCUtil;
 
 /**
  * 给定一个未排序的整数数组，找到最长递增子序列的个数。
  * 注意: 给定的数组长度不超过 2000 并且结果一定是32位有符号整数。
  * 提示:
- * 1、1 <= n <= 2000
+ * 1、1 <= nums.length <= 2000
  * 2、-10^6 <= nums[i] <= 10^6
  * 链接：https://leetcode-cn.com/problems/number-of-longest-increasing-subsequence/
  */
@@ -18,37 +18,25 @@ public class Q673 {
 
     public void TestOJ() {
         // 1
-        System.out.println(findNumberOfLIS(stringToIntegerArray("[3,1,2]")));
+        System.out.println(findNumberOfLIS(LCUtil.stringToIntegerArray("[3,1,2]")));
         // 2
-        System.out.println(findNumberOfLIS(stringToIntegerArray("[1,3,5,4,7]")));
+        System.out.println(findNumberOfLIS(LCUtil.stringToIntegerArray("[1,3,5,4,7]")));
         // 5
-        System.out.println(findNumberOfLIS(stringToIntegerArray("[2,2,2,2,2]")));
+        System.out.println(findNumberOfLIS(LCUtil.stringToIntegerArray("[2,2,2,2,2]")));
     }
 
     public int findNumberOfLIS(int[] nums) {
-        int n = nums.length, maxLen = 0, ans = 0;
-        int[] dp = new int[n];
-        int[] cnt = new int[n];
-        for (int i = 0; i < n; ++i) {
-            dp[i] = 1;
-            cnt[i] = 1;
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
-                    if (dp[j] + 1 > dp[i]) {
-                        dp[i] = dp[j] + 1;
-                        cnt[i] = cnt[j]; // 重置计数
-                    } else if (dp[j] + 1 == dp[i]) {
-                        cnt[i] += cnt[j];
-                    }
-                }
-            }
-            if (dp[i] > maxLen) {
-                maxLen = dp[i];
-                ans = cnt[i]; // 重置计数
-            } else if (dp[i] == maxLen) {
-                ans += cnt[i];
-            }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int out = 1, max = 1;
+        for (int i = 1; i < nums.length; i++) {
+            int p = i, len = 1;
+            while (p >= 0 && nums[p] >= nums[i]) p--;
+            if (p != -1) len = dp[p] + 1;
+            dp[i] = Math.max(dp[i - 1], len);
+            if (len > max) max = len;
+            else if (len == max) out++;
         }
-        return ans;
+        return out;
     }
 }
