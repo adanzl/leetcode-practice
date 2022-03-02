9ed12b129c89bde4b7602902fd1f28751f77c9ce
zhaolin09 <zhaolin09@58.com>
2022-03-02 10:56:51 +0800

    Q1994

--- src/com/leo/leetcode/algorithm/q1900/Q1994.java
+++ src/com/leo/leetcode/algorithm/q1900/Q1994.java
@@ -1,8 +1,5 @@
 package com.leo.leetcode.algorithm.q1900;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static com.leo.utils.LCUtil.stringToIntegerArray;
 
 /**
@@ -20,18 +17,20 @@ import static com.leo.utils.LCUtil.stringToIntegerArray;
 public class Q1994 {
 
     public static void main(String[] args) {
-        // 0
-        System.out.println(new Q1994().numberOfGoodSubsets(stringToIntegerArray("[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]")));
-        // 8
-        System.out.println(new Q1994().numberOfGoodSubsets(stringToIntegerArray("[1,1,1,2]")));
+        // 5
+        System.out.println(new Q1994().numberOfGoodSubsets(stringToIntegerArray("[5,6,6]")));
         // 5368
         System.out.println(new Q1994().numberOfGoodSubsets(stringToIntegerArray("[5,10,1,26,24,21,24,23,11,12,27,4,17,16,2,6,1,1,6,8,13,30,24,20,2,19]")));
-        // 19
-        System.out.println(new Q1994().numberOfGoodSubsets(stringToIntegerArray("[18,28,2,17,29,30,15,9,12]")));
-        // 6
-        System.out.println(new Q1994().numberOfGoodSubsets(stringToIntegerArray("[1,2,3,4]")));
         // 6
         System.out.println(new Q1994().numberOfGoodSubsets(stringToIntegerArray("[1,2,3]")));
+        // 19
+        System.out.println(new Q1994().numberOfGoodSubsets(stringToIntegerArray("[18,28,2,17,29,30,15,9,12]")));
+        // 8
+        System.out.println(new Q1994().numberOfGoodSubsets(stringToIntegerArray("[1,1,1,2]")));
+        // 0
+        System.out.println(new Q1994().numberOfGoodSubsets(stringToIntegerArray("[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]")));
+        // 6
+        System.out.println(new Q1994().numberOfGoodSubsets(stringToIntegerArray("[1,2,3,4]")));
         // 2
         System.out.println(new Q1994().numberOfGoodSubsets(stringToIntegerArray("[1,2]")));
         // 5
@@ -40,48 +39,57 @@ public class Q1994 {
         System.out.println(new Q1994().numberOfGoodSubsets(stringToIntegerArray("[2,3]")));
     }
 
+    static final int[] PRIMES = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+    static final int NUM_MAX = 30;
+    static final int MOD = 1000000007;
+
     public int numberOfGoodSubsets(int[] nums) {
-        List<Integer> primeSigns = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
-        int countOne = 0, MOD = 1000000007, ret;
+        int[] freq = new int[NUM_MAX + 1];
         for (int num : nums) {
-            int prime = primeSign(num);
-            if (prime != 0) primeSigns.add(prime);
-            if (num == 1) countOne++;
+            ++freq[num];
         }
-        ans.add(0);
-        for (Integer primeSign : primeSigns) {
-            goodSubsets(ans, primeSign);
-        }
-        ret = ans.size() - (int)Math.pow(2, countOne);
-        return ret % MOD;
-    }
 
-    int primeSign(int num) {
-        if (num == 1) return 1;
-        int[] prime = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-        int ret = 0;
-        for (int i = 0; i < prime.length; ) {
-            int p = prime[i];
-            if (num % p == 0) {
-                num /= p;
-                if ((ret & 1 << (p - 1)) != 0) return 0;
-                ret |= (1 << (p - 1));
-            } else {
-                i++;
+        int[] f = new int[1 << PRIMES.length];
+        f[0] = 1;
+        for (int i = 0; i < freq[1]; ++i) {
+            f[0] = f[0] * 2 % MOD;
+        }
+
+        for (int i = 2; i <= NUM_MAX; ++i) {
+            if (freq[i] == 0) {
+                continue;
+            }
+
+            // 检查 i 的每个质因数是否均不超过 1 个
+            int subset = 0;
+            boolean check = true;
+            for (int j = 0; j < PRIMES.length; ++j) {
+                int prime = PRIMES[j];
+                if (i % (prime * prime) == 0) {
+                    check = false;
+                    break;
+                }
+                if (i % prime == 0) {
+                    subset |= (1 << j);
+                }
+            }
+            if (!check) {
+                continue;
+            }
+
+            // 动态规划
+            for (int mask = (1 << PRIMES.length) - 1; mask > 0; --mask) {
+                if ((mask & subset) == subset) {
+                    f[mask] = (int) ((f[mask] + ((long) f[mask ^ subset]) * freq[i]) % MOD);
+                }
             }
         }
-        return ret;
-    }
 
-
-    void goodSubsets(List<Integer> ans, int v) {
-        int limit = ans.size();
-        for (int i = 0; i < limit; i++) {
-            int pre = ans.get(i);
-            if ((pre & v) == 0 || v == 1) {
-                ans.add(pre | v);
-            }
+        int ans = 0;
+        for (int mask = 1, maskMax = (1 << PRIMES.length); mask < maskMax; ++mask) {
+            ans = (ans + f[mask]) % MOD;
         }
+
+        return ans;
     }
 }
