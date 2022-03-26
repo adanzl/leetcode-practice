939d474fde3c1617cb3a4370a6ff0a00cb20d72d
zhaolin09 <zhaolin09@58.com>
2022-03-26 17:15:05 +0800

    Q493-利用归并特性

--- src/com/leo/leetcode/algorithm/q0400/Q493.java
+++ src/com/leo/leetcode/algorithm/q0400/Q493.java
@@ -13,57 +13,44 @@ import static com.leo.utils.LCUtil.stringToIntegerArray;
  * 链接：https://leetcode-cn.com/problems/reverse-pairs
  */
 public class Q493 {
-
+    // 利用归并特性
     public static void main(String[] args) {
-        // 624975000
-        TestCase tc = new TestCase("resources/Q493/Case001.txt");
-        System.out.println(new Q493().reversePairs(stringToIntegerArray(tc.getData(0))));
+        // 0
+        System.out.println(new Q493().reversePairs(stringToIntegerArray("[2147483647,2147483647,2147483647,2147483647,2147483647,2147483647]")));
         // 2
         System.out.println(new Q493().reversePairs(stringToIntegerArray("[1,3,2,3,1]")));
         // 3
         System.out.println(new Q493().reversePairs(stringToIntegerArray("[2,4,3,5,1]")));
+        // 624975000
+        TestCase tc = new TestCase("resources/Q493/Case001.txt");
+        System.out.println(new Q493().reversePairs(stringToIntegerArray(tc.getData(0))));
     }
 
     public int reversePairs(int[] nums) {
-        int ret = 0;
-        Node head = new Node(Integer.MIN_VALUE, Integer.MAX_VALUE);
-        for (long n : nums) {
-            ret += query(head, (n << 1) + 1, Integer.MAX_VALUE);
-            addNode(head, n);
+        return merge(nums, 0, nums.length, new int[nums.length]);
+    }
+
+    int merge(int[] nums, int s, int e, int[] tmp) {
+        if (s == e - 1) return 0;
+        int mid = (s + e) >> 1, ret = 0;
+        ret += merge(nums, s, mid, tmp);
+        ret += merge(nums, mid, e, tmp);
+        int l = s, r = mid;
+        while (l < mid && r < e) {
+            while (nums[l] <= ((long) nums[r] << 1) && l < mid) l++;
+            ret += mid - l;
+            r++;
         }
+
+        int i0 = s, i1 = mid, idx = 0;
+        for (; i0 < mid && i1 < e; idx++) {
+            if (nums[i0] < nums[i1]) tmp[idx] = nums[i0++];
+            else tmp[idx] = nums[i1++];
+        }
+        while (i0 < mid) tmp[idx++] = nums[i0++];
+        while (i1 < e) tmp[idx++] = nums[i1++];
+        System.arraycopy(tmp, 0, nums, s, e - s);
         return ret;
     }
 
-    void addNode(Node root, long v) {
-        root.c++;
-        if (root.l == root.r) return;
-        long mid = (root.r + root.l) >> 1;
-        if (v <= mid) {
-            if (root.left == null) root.left = new Node(root.l, mid);
-            addNode(root.left, v);
-        } else {
-            if (root.right == null) root.right = new Node(mid + 1, root.r);
-            addNode(root.right, v);
-        }
-    }
-
-    int query(Node root, long l, long r) {
-        if (root == null || l > root.r || r < root.l) return 0;
-        if (root.l == l && r == root.r) return root.c;
-        long mid = (root.r + root.l) >> 1;
-        if (r <= mid) return query(root.left, l, r);
-        if (l >= mid + 1) return query(root.right, l, r);
-        return query(root.left, l, mid) + query(root.right, mid + 1, r);
-    }
-
-    static class Node {
-        long l, r;
-        int c = 0;
-        Node left, right;
-
-        Node(long l, long r) {
-            this.l = l;
-            this.r = r;
-        }
-    }
 }
