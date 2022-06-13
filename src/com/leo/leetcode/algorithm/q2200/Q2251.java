a455e557790d05ee9ce225a8f2ac296bdd68dad1
zhaolin09 <zhaolin09@58.com>
2022-06-14 01:37:14 +0800

    Q2251-opt bSearch comment

--- src/com/leo/leetcode/algorithm/q2200/Q2251.java
+++ src/com/leo/leetcode/algorithm/q2200/Q2251.java
@@ -45,7 +45,7 @@ public class Q2251 {
         int l = 0, r = arr.length - 1;
         while (l <= r) {
             int mid = l + (r - l) / 2;
-            if (arr[mid] > x) r = mid - 1;
+            if (arr[mid] > x) r = mid - 1; // 相同元素返回右边 修改为 >= 后插入左边
             else l = mid + 1;
         }
         return l;
