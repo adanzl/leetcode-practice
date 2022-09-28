d8fa3151b955a1c8e614d027b1bc3f8b3d683c68
zhaolin09 <zhaolin09@58.com>
2022-09-28 10:30:56 +0800

    rename dir

---
+++ py/q0800/Q818.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+
+class Solution:
+    def race_car(self, target: int) -> int:
+        return 0
+
+if __name__ == '__main__':
+    # 2
+    print(Solution().race_car(3))
+    # 5
+    print(Solution().race_car(6))
+    #
+    # print(Solution().race_car(1))
