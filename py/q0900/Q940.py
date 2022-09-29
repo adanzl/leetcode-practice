c530a9a03412479bbcc01a48cf72c8eca3d477eb
zhaolin09 <zhaolin09@58.com>
2022-09-29 10:33:34 +0800

    Q940-py

---
+++ py/q0900/Q940.py
@@ -0,0 +1,29 @@
+"""
+ * 给定一个字符串 s，计算 s 的 不同非空子序列 的个数。因为结果可能很大，所以返回答案需要对 10^9 + 7 取余 。
+ * 字符串的 子序列 是经由原字符串删除一些（也可能不删除）字符但不改变剩余字符相对位置的一个新字符串。
+ * 例如，"ace" 是 "abcde" 的一个子序列，但 "aec" 不是。
+ * 提示：
+ * 1、1 <= s.length <= 2000
+ * 2、s 仅由小写英文字母组成
+ * 链接：https://leetcode.cn/problems/distinct-subsequences-ii/
+"""
+from typing import *
+
+
+class Solution:
+
+    def distinctSubseqII(self, s: str) -> int:
+        MOD = 10**9 + 7
+        dp = [0] * 26
+        for c in s:
+            dp[ord(c) - ord('a')] = sum(dp, 1) % MOD
+        return sum(dp) % MOD
+
+
+if __name__ == '__main__':
+    # 7
+    print(Solution().distinctSubseqII("abc"))
+    # 6
+    print(Solution().distinctSubseqII("aba"))
+    # 3
+    print(Solution().distinctSubseqII("aaa"))
