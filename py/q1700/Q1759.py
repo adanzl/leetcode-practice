9c6181f726079b01dabda13d6c783eed86b277e6
zhaolin <adanzl@163.com>
2022-12-26 10:37:50 +0800

    groupby~

--- py/q1700/Q1759.py
+++ py/q1700/Q1759.py
@@ -8,8 +8,17 @@
  * 链接：https://leetcode.cn/problems/count-number-of-homogenous-substrings/
 """
 
+from itertools import groupby
+
 
 class Solution:
+
+    def countHomogenous1(self, s: str) -> int:
+        res = 0
+        for k, g in groupby(s):
+            n = len(list(g))
+            res += (n + 1) * n // 2
+        return res % (10**9 + 7)
 
     def countHomogenous(self, s: str) -> int:
         MOD = 10**9 + 7
