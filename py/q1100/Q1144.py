066bb98dc184f368cddd3d41761a7faab4e219b5
zhaolin <adanzl@163.com>
2023-03-04 13:49:11 +0800

    Q1144-py

--- py/q1100/Q1144.py
+++ py/q1100/Q1144.py
@@ -17,7 +17,7 @@ class Solution:
     def movesToMakeZigzag(self, nums):
         s = [0] * 2
         for i, x in enumerate(nums):
-            left = nums[i - 1] if i else float('inf')
+            left = nums[i - 1] if i else float('inf')  
             right = nums[i + 1] if i < len(nums) - 1 else float('inf')
             s[i % 2] += max(x - min(left, right) + 1, 0)
         return min(s)
