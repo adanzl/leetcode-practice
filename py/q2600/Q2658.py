8b3bc5cbc5e83f5001bb6f50b82ff0590429e41e
zhaolin <adanzl@163.com>
2023-04-30 23:45:39 +0800

    Q2658-py

--- py/q2600/Q2658.py
+++ py/q2600/Q2658.py
@@ -41,5 +41,7 @@ class Solution:
 
 
 if __name__ == '__main__':
-    #
-    print(Solution().func())
+    # 7
+    print(Solution().findMaxFish([[0, 2, 1, 0], [4, 0, 0, 3], [1, 0, 0, 4], [0, 3, 2, 0]]))
+    # 1
+    print(Solution().findMaxFish([[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]]))
