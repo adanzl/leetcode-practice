28bcfbd6d7a98798c0e281e2605845db07871332
zhaolin <adanzl@163.com>
2023-09-27 00:14:19 +0800

    OPT

--- py/q1300/Q1333.py
+++ py/q1300/Q1333.py
@@ -7,12 +7,12 @@
  * 过滤后返回餐馆的 id，按照 rating 从高到低排序。如果 rating 相同，那么按 id 从高到低排序。
  * 简单起见， veganFriendly_i 和 veganFriendly 为 true 时取值为 1，为 false 时，取值为 0 。
  * 提示：
- * 1 <= restaurants.length <= 10^4
- * restaurants[i].length == 5
- * 1 <= id_i, rating_i, price_i, distance_i <= 10^5
- * 1 <= maxPrice, maxDistance <= 10^5
- * veganFriendly_i 和 veganFriendly 的值为 0 或 1 。
- * 所有 idi 各不相同。
+ * 1、1 <= restaurants.length <= 10^4
+ * 2、restaurants[i].length == 5
+ * 3、1 <= id_i, rating_i, price_i, distance_i <= 10^5
+ * 4、1 <= maxPrice, maxDistance <= 10^5
+ * 5、veganFriendly_i 和 veganFriendly 的值为 0 或 1 。
+ * 6、所有 id_i 各不相同。
  * 链接：https://leetcode.cn/problems/filter-restaurants-by-vegan-friendly-price-and-distance
 """
 
