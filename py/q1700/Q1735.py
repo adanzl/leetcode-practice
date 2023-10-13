33558b614099bb4de26870b1ae6c498ea60969a4
zhaolin <adanzl@163.com>
2023-10-13 10:21:27 +0800

    Q1735-py

--- py/q1700/Q1735.py
+++ py/q1700/Q1735.py
@@ -1,49 +1,55 @@
 """
+ * 给你一个二维整数数组 queries ，其中 queries[i] = [ni, ki] 。
+ * 第 i 个查询 queries[i] 要求构造长度为 ni 、每个元素都是正整数的数组，且满足所有元素的乘积为 ki ，请你找出有多少种可行的方案。
+ * 由于答案可能会很大，方案数需要对 10^9 + 7 取余 。
+ * 请你返回一个整数数组 answer，满足 answer.length == queries.length ，其中 answer[i]是第 i 个查询的结果。
+ * 提示：
+ * 1、1 <= queries.length <= 10^4 
+ * 2、1 <= ni, ki <= 10^4
  * 链接：https://leetcode.cn/problems/count-ways-to-make-array-with-product/
 """
-from functools import cache
+from collections import Counter
+from math import comb
 from typing import List
-# import sys
 
-# sys.setrecursionlimit(5 * 10**5)
+#
+# @lc app=leetcode.cn id=1735 lang=python3
+#
+# [1735] 生成乘积数组的方案数
+#
+
+# @lc code=start
+
+# 约数 预处理
+divisors = [[], []]
+for i in range(2, 10**4 + 1):
+    for j in range(2, int(i**0.5) + 1):
+        if i % j == 0:
+            divisors.append(divisors[i // j] + [j])
+            break
+    else:
+        divisors.append([i])
 
 
 class Solution:
 
     def waysToFillArray(self, queries: List[List[int]]) -> List[int]:
-        MOD = 10**9 + 7
-        # 约数 预处理
-        N = 105
-        divisors = [[] for _ in range(N)]
-        for i in range(2, N):
-            for j in range(i, N, i):
-                divisors[j].append(i)
-        dp = [[0] * N for _ in range(N)]  # dp[i][j] i个数 乘积是j
-        for i in range(N):
-            dp[1][i] = 1
-        for i in range(2, N):  # i th
-            for j in range(1, N):  # end with j
-                for l in range(1, N // j):
-                    dp[i][l * j] += dp[i - 1][l]
-        # @cache
-        # def f(n, k):
-        #     if k == 1 or n == 1: return 1
-        #     ret = f(n - 1, k)
-        #     for d in divisors[k]:
-        #         ret += f(n - 1, k // d)
-        #     return ret
-
-        ans = []
-        for ni, ki in queries:
-            # ans.append(f(ni, ki) % MOD)
-            ans.append(dp[ni][ki] % MOD)
-        return ans
+        res = []
+        for i, j in queries:
+            c = Counter(divisors[j])
+            x = 1
+            for k in c.values():
+                x *= comb(i + k - 1, k)
+                x %= 10**9 + 7
+            res.append(x % (10**9 + 7))
+        return res
 
 
+# @lc code=end
 if __name__ == '__main__':
     # [4,1,50734910]
     print(Solution().waysToFillArray([[2, 6], [5, 1], [73, 660]]))
     # [1,2,3,10,5]
     print(Solution().waysToFillArray([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]]))
-    #
-    # print(Solution().waysToFillArray([[2, 6], [5, 1], [10000, 10000]]))
+    # [4, 1, 418224726]
+    print(Solution().waysToFillArray([[2, 6], [5, 1], [10000, 10000]]))
