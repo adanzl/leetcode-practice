7968f950f9374e4e021412df7684c3f710e62261
zhaolin <adanzl@163.com>
2024-03-19 12:29:17 +0800

    Q1793-py 增加单调栈解法

--- py/q1700/Q1793.py
+++ py/q1700/Q1793.py
@@ -14,7 +14,8 @@ from typing import List
 
 class Solution:
 
-    def maximumScore(self, nums: List[int], k: int) -> int:
+    def maximumScore1(self, nums: List[int], k: int) -> int:
+        # 差分数组
         ans, n = 0, len(nums)
         LIMIT = max(nums) + 5
         diff_l, diff_r = [0] * LIMIT, [0] * LIMIT  # 差分
@@ -36,13 +37,37 @@ class Solution:
             ans = max(ans, (v_l + v_r - 1) * i)
         return ans
 
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        # 单调栈
+        ans, n = 0, len(nums)
+        s_l, s_r = [], []
+        for i in range(k + 1):
+            while s_l and nums[s_l[-1]] > nums[i]:
+                s_l.pop()
+            s_l.append(i)
+        for i in range(n - 1, k - 1, -1):
+            while s_r and nums[s_r[-1]] > nums[i]:
+                s_r.pop()
+            s_r.append(i)
+        LIMIT = max(nums[s_l[-1]], nums[s_r[-1]])
+        for v in range(LIMIT, 0, -1):
+            i_l, i_r = -1, n
+            while s_l and nums[s_l[-1]] >= v:
+                s_l.pop()
+            if s_l: i_l = s_l[-1]
+            while s_r and nums[s_r[-1]] >= v:
+                s_r.pop()
+            if s_r: i_r = s_r[-1]
+            ans = max(ans, (i_r - i_l - 1) * v)
+        return ans
+
 
 if __name__ == '__main__':
+    # 15
+    print(Solution().maximumScore([1, 4, 3, 7, 4, 5], 3))
     # 12
     print(Solution().maximumScore([7, 5, 4], 1))
     # 9732
     print(Solution().maximumScore([6569, 9667, 3148, 7698, 1622, 2194, 793, 9041, 1670, 1872], 5))
-    # 15
-    print(Solution().maximumScore([1, 4, 3, 7, 4, 5], 3))
     # 20
     print(Solution().maximumScore([5, 5, 4, 5, 4, 1, 1, 1], 0))
